15ef14a8d1a57565217c537b989c11a79a85ceee
jrivera604 <joshrivera91@hotmail.com>
2023-04-25 18:42:47 +0000

    add initialize method of the game class to create two players

--- main.rb
+++ main.rb
@@ -1,2 +1,12 @@
 require_relative 'player'
 require_relative 'question'
+
+class Game
+  def initialize
+    @players = [Player.new('Player 1'), Player.new('Player 2')]
+    @current_player = @players[0]
+    @game_over = false
+  end
+
+end
+
